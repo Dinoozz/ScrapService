0b1e5429dabe83697dd88a322b65d3ac34622562
CamilleLecoq <55230689+EstebanEpitech@users.noreply.github.com>
2023-11-23 18:16:12 +0100

    [+]

--- mongodb_link.js
+++ mongodb_link.js
@@ -5,9 +5,9 @@ const MONGODB_URI = process.env.MONGODB_URI;
 const mongoose = require('mongoose');
  // Remplacez avec votre URI de MongoDB
 
-const connectDB = async () => {
+const connectDB = () => {
     try {
-        await mongoose.connect(MONGODB_URI,{ auth: { authSource: "admin" } });;
+        mongoose.connect(MONGODB_URI);
         console.log('MongoDB connecté...');
     } catch (err) {
         console.error(err.message);
